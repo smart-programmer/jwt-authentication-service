c8b2ecd9cecfb713615a0db1c775805548c00ab7
smart-programmer <albrns1123488@outlook.com>
2022-10-17 23:29:24 +0300

    change API_KEY structure

--- api_security/api_user_auth.js
+++ api_security/api_user_auth.js
@@ -2,48 +2,61 @@ var CryptoJS = require("crypto-js");
 const { request } = require("express");
 const prompt = require('prompt-sync')({ sigint: true });
 const path = require('path')
+const jwt = require('jsonwebtoken')
 require('dotenv').config({ path: path.join(__dirname, '../.env') })
 
 
 
-function getJsonFromToken(token){
+function extractJwt(encryptedToken){
     try {
-        token = token.slice(12, token.length) // remove api_auth_st_
-        let base64DecodedString = Buffer.from(token, 'base64').toString('utf8') // decode from base64 back to uf8
+        // remove api_auth_st_
+        encryptedToken = encryptedToken.slice(12, encryptedToken.length) 
+        // decode from base64 back to utf8
+        let utf8DecodedString = Buffer.from(encryptedToken, 'base64').toString('utf8')
         // decode AES encryption
-        let bytes  = CryptoJS.AES.decrypt(base64DecodedString, process.env.TEMP_SITE_SECRET_KEY);
-        let originalText = bytes.toString(CryptoJS.enc.Utf8);
-        let originalJson = JSON.parse(originalText)
-        return originalJson
+        let bytes  = CryptoJS.AES.decrypt(utf8DecodedString, process.env.TEMP_SITE_SECRET_KEY);
+        let originalJwt = bytes.toString(CryptoJS.enc.Utf8);
+        return originalJwt
     } catch (e) {
         return null
     }
 }
 
-function checkAuthorizedUserMiddleware(req, res, next) {
-    let authorizationHeader = req.headers.authorization
-    if (!authorizationHeader) {
-        res.status(401).send("you're unauthorized to access this page.");
-        return;
-    }
-    let token = getTokenFromRequestHeader(authorizationHeader)
-    let tokenJson = getJsonFromToken(token)
-    // if it's a valid json object then user is authorized
-    if (!tokenJson) {
-        res.status(401).send("you're unauthorized to access this page.");
-        return;
-    }
-    console.log(tokenJson)
-    next()
-}
-
-function getTokenFromRequestHeader(authorizationHeader) {
-    let token = authorizationHeader.split(' ')[1] // bearer token
+function getKeyFromRequestHeader(authorizationHeader) {
+    let token = authorizationHeader.split(' ')[1] // "bearer token" no spaces after token
     return token
 }
 
+function checkAuthorizedUserMiddleware(req, res, next) {
+    let authorizationHeader = req.headers.authorization
+    // check if header exists
+    if (!authorizationHeader) {
+        console.log("no authorization header found")
+        res.status(401).send("you're unauthorized to access this page.");
+        return;
+    }
+    let encryptedToken = getKeyFromRequestHeader(authorizationHeader)
+    let jwtToken = extractJwt(encryptedToken)
+    // if not valid jwt token then unauthorized
+    if (!jwtToken) {
+        console.log("decryption error: invalid jwt token")
+        res.status(401).send("you're unauthorized to access this page.");
+        return;
+    }
+    // check for signature
+    try {
+        const jwtPayload = jwt.verify(jwtToken, process.env.TEMP_SITE_SECRET_KEY);
+        console.log(jwtPayload)
+    } catch (e) {
+        console.log("incorrect or expired jwt token")
+        res.status(401).send("you're unauthorized to access this page.");
+        return;
+    }
+    next()
+}
+
+
 
 module.exports = {
-    getJsonFromToken: getJsonFromToken,
     checkAuthorizedUserMiddleware: checkAuthorizedUserMiddleware
 }
--- api_security/key_generation_tool.js
+++ api_security/key_generation_tool.js
@@ -2,36 +2,43 @@ const AES = require('crypto-js/aes')
 const prompt = require('prompt-sync')({ sigint: true });
 const randomstring = require("randomstring");
 const path = require('path')
+const jwt = require('jsonwebtoken')
 require('dotenv').config({ path: path.join(__dirname, '../.env') })
 
 
-
+/* build jwt token */
 console.log("\nplease answer the following questions as they will be stored in the api key\n")
-
 
 let expirationPeriod = prompt("Enter expiration period in seconds, skip to make permanent: ")
 if (expirationPeriod){
     expirationPeriod = parseInt(expirationPeriod)
 } else {
-    expirationPeriod = 0
+    expirationPeriod = 60 * 60 * 24 * 365 * 20 // default expires in a 20 years
 }
 
 let creatorName = prompt("Enter your name: ")
-let createdAt = new Date()
 let target = prompt("why are you generating this key for?: ")
 
-const tokenData = {
-    expirationPeriod: expirationPeriod,
+const jwtPayload = {
     creatorName: creatorName,
-    createdAt: createdAt,
     target: target,
     randomString: randomstring.generate(7)
 }
 
-const cipherText = AES.encrypt(JSON.stringify(tokenData), process.env.TEMP_SITE_SECRET_KEY).toString();
+// generate jwt
+const jwtToken = jwt.sign(jwtPayload, process.env.TEMP_SITE_SECRET_KEY, {
+    expiresIn: expirationPeriod
+})
+
+// encrypt jwt
+const cipherText = AES.encrypt(jwtToken, process.env.TEMP_SITE_SECRET_KEY).toString();
+
+// base 64 encode the cipher 
 const base64Token = Buffer.from(cipherText).toString('base64')
-const finalKey = "api_auth_st_" + base64Token // first word short for api secret token
-console.log(finalKey)
+
+const finalToken = "api_auth_st_" + base64Token // first word short for api secret token
+
+console.log(finalToken)
 
 
 
