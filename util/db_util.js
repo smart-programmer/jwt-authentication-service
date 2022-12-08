d2e24a40f810919b805257a5480a4e7b9d17d213
smart-programmer <albrns1123488@outlook.com>
2022-12-08 13:29:16 +0300

    delte mockup stuff

--- util/db_util.js
+++ util/db_util.js
@@ -19,39 +19,6 @@ async function getAllUsers() {
     return users
 }
 
-// async function replaceUser(oldUserQuery, newUser) {
-//     const dbConnection = await mongooseConnect()
-//     // delete old user
-//     await UserModel.deleteOne(oldUserQuery)
-//     // add new user to db
-//     const instance = new UserModel(newUser);
-//     instance.save((err, user) => {
-//         if (err) {
-//             console.log(err)
-//             console.error("couldn't add user")
-//             dbConnection.close()
-//             return
-//         }
-//         dbConnection.close()
-//         console.log("user added successfully")
-//     })
-// }
-
-
-// findUser({username: "Ahmad"}).then(async user => {
-//     if (!user){
-//         console.log("user not found")
-//         return
-//     }
-//     user.username = "Ahmad"
-//     user._id = mongoose.Types.ObjectId()
-//     await replaceUser({username: "Ahmad"}, user)
-//     findUser({username: "Ahmad"}).then(newUser => {
-//         console.log(newUser)
-//     })
-// })
-
-
 module.exports = {
     findUser : findUser,
     getAllUsers : getAllUsers,
